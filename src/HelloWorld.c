dcad5ba1ee3c34cf2c295e0117c8b7a345a2fbf6
Rocky Gray Jr <rocky@videoblocks.com>
2014-12-25 23:34:19 -0500

    use font

--- src/HelloWorld.c
+++ src/HelloWorld.c
@@ -2,15 +2,16 @@
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static GFont s_time_font;
 
 static void update_time() {
     // Get a tm structure
     time_t temp = time(NULL);
     struct tm *tick_time = localtime(&temp);
-    
+
     // Create a long-lived buffer
     static char buffer[] = "00:00";
-    
+
     char *format = "";
     if(clock_is_24h_style() == true) {
         format = "%H:%M";
@@ -18,7 +19,7 @@ static void update_time() {
         format = "%I:%M";
     }
     strftime(buffer, sizeof("00:00"), format, tick_time);
-    
+
     text_layer_set_text(s_time_layer, buffer);
 }
 
@@ -27,17 +28,20 @@ static void main_window_load(Window *window) {
     s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
     text_layer_set_background_color(s_time_layer, GColorClear);
     text_layer_set_text_color(s_time_layer, GColorBlack);
-    
+
+    s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_PERFECT_DOS_VGA_42));
+
     // Improve the layout to be more like a watchface
-    text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+    text_layer_set_font(s_time_layer, s_time_font);
     text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-    
+
     // Add it as a child layer to the Window's root layer
-    layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));  
+    layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
 }
 
 static void main_window_unload(Window *window) {
     text_layer_destroy(s_time_layer);
+    fonts_unload_custom_font(s_time_font);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -46,16 +50,16 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 
 static void init() {
     s_main_window = window_create();
-    
+
     // set handlers to manage the elements inside the Window
     window_set_window_handlers(s_main_window, (WindowHandlers) {
         .load = main_window_load,
         .unload = main_window_unload
     });
-    
+
     // Register with TickTimerService
     tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
-    
+
     // Show the window on the watch, with animated=true
     window_stack_push(s_main_window, true);
     // Make sure the time is displayed from the start
@@ -68,10 +72,10 @@ static void deinit() {
 
 int main(void) {
     init();
-    
+
     // letts the watchapp wait for system events until it exits
     app_event_loop();
-    
+
     deinit();
 }
 
