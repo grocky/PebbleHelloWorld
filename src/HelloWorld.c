228838926833bf717ea6bf9949063f17e1637cc2
Rocky Gray <rocky.grayjr@gmail.com>
2014-12-25 20:34:51 -0500

    Show the real time; first step complete :bowtie:

--- src/HelloWorld.c
+++ src/HelloWorld.c
@@ -2,13 +2,31 @@
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+
+static void update_time() {
+    // Get a tm structure
+    time_t temp = time(NULL);
+    struct tm *tick_time = localtime(&temp);
     
+    // Create a long-lived buffer
+    static char buffer[] = "00:00";
+    
+    char *format = "";
+    if(clock_is_24h_style() == true) {
+        format = "%H:%M";
+    } else {
+        format = "%I:%M";
+    }
+    strftime(buffer, sizeof("00:00"), format, tick_time);
+    
+    text_layer_set_text(s_time_layer, buffer);
+}
+
 static void main_window_load(Window *window) {
     // Create time TextLayer
     s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
     text_layer_set_background_color(s_time_layer, GColorClear);
     text_layer_set_text_color(s_time_layer, GColorBlack);
-    text_layer_set_text(s_time_layer, "00:00");
     
     // Improve the layout to be more like a watchface
     text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
@@ -22,6 +40,10 @@ static void main_window_unload(Window *window) {
     text_layer_destroy(s_time_layer);
 }
 
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+    update_time();
+}
+
 static void init() {
     s_main_window = window_create();
     
@@ -31,8 +53,13 @@ static void init() {
         .unload = main_window_unload
     });
     
+    // Register with TickTimerService
+    tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+    
     // Show the window on the watch, with animated=true
     window_stack_push(s_main_window, true);
+    // Make sure the time is displayed from the start
+    update_time();
 }
 
 static void deinit() {
