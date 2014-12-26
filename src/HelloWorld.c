61aaef11abeb2bf766ddd221da020e92dfd7c04e
Rocky Gray Jr <rocky@videoblocks.com>
2014-12-26 01:19:59 -0500

    re-add changes for AppMessage in HelloWorld.c

--- src/HelloWorld.c
+++ src/HelloWorld.c
@@ -1,16 +1,28 @@
 #include <pebble.h>
 
+#define KEY_TEMERATURE 0
+#define KEY_CONDITIONS 1
+
 static Window *s_main_window;
+
 static TextLayer *s_time_layer;
+static TextLayer *s_weather_layer;
+
+static GFont s_time_font;
+static GFont s_weather_font;
+
+static BitmapLayer  *s_background_layer;
+
+static GBitmap *s_background_bitmap;
 
 static void update_time() {
     // Get a tm structure
     time_t temp = time(NULL);
     struct tm *tick_time = localtime(&temp);
-    
+
     // Create a long-lived buffer
     static char buffer[] = "00:00";
-    
+
     char *format = "";
     if(clock_is_24h_style() == true) {
         format = "%H:%M";
@@ -18,46 +30,97 @@ static void update_time() {
         format = "%I:%M";
     }
     strftime(buffer, sizeof("00:00"), format, tick_time);
-    
+
     text_layer_set_text(s_time_layer, buffer);
 }
 
 static void main_window_load(Window *window) {
+    // Ceate GBitmap, then set to created BitmapLayer
+    s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BACKGROUND);
+    s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
+    bitmap_layer_set_bitmap(s_background_layer,s_background_bitmap);
+    layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
+
     // Create time TextLayer
-    s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
+    s_time_layer = text_layer_create(GRect(2, 55, 144, 50));
     text_layer_set_background_color(s_time_layer, GColorClear);
     text_layer_set_text_color(s_time_layer, GColorBlack);
-    
+
+    s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_PERFECT_DOS_VGA_42));
+
     // Improve the layout to be more like a watchface
-    text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+    text_layer_set_font(s_time_layer, s_time_font);
     text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-    
+
+    // Create temperature Layer
+    s_weather_layer = text_layer_create(GRect(0, 130, 144, 25));
+    text_layer_set_background_color(s_weather_layer, GColorClear);
+    text_layer_set_text_color(s_weather_layer, GColorWhite);
+    text_layer_set_text_alignment(s_weather_layer, GTextAlignmentCenter);
+    text_layer_set_text(s_weather_layer, "Ashley...");
+
+    // Create second custom font, apply it and add to Window
+    s_weather_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_PERFECT_DOS_VGA_20));
+    text_layer_set_font(s_weather_layer, s_weather_font);
+    layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_weather_layer));
+
     // Add it as a child layer to the Window's root layer
-    layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));  
+    layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
 }
 
 static void main_window_unload(Window *window) {
     text_layer_destroy(s_time_layer);
+    fonts_unload_custom_font(s_time_font);
+    gbitmap_destroy(s_background_bitmap);
+    bitmap_layer_destroy(s_background_layer);
+
+    // destroy weather elements
+    text_layer_destroy(s_weather_layer);
+    fonts_unload_custom_font(s_weather_font);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
     update_time();
 }
 
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped.");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed.");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+    APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send successful.");
+}
+
 static void init() {
     s_main_window = window_create();
-    
+
     // set handlers to manage the elements inside the Window
     window_set_window_handlers(s_main_window, (WindowHandlers) {
         .load = main_window_load,
         .unload = main_window_unload
     });
-    
+
     // Register with TickTimerService
     tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
-    
+
+    // Register callbacks
+    app_message_register_inbox_received(inbox_received_callback);
+    app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+    app_message_register_inbox_dropped(inbox_dropped_callback);
+    app_message_register_outbox_failed(outbox_failed_callback);
+    app_message_register_outbox_sent(outbox_sent_callback);
+
     // Show the window on the watch, with animated=true
     window_stack_push(s_main_window, true);
+
     // Make sure the time is displayed from the start
     update_time();
 }
@@ -68,10 +131,10 @@ static void deinit() {
 
 int main(void) {
     init();
-    
+
     // letts the watchapp wait for system events until it exits
     app_event_loop();
-    
+
     deinit();
 }
 
