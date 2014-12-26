646e8857c4ad220769f1dc8bdc52016fbe8f143a
Rocky Gray Jr <rocky@videoblocks.com>
2014-12-26 00:00:47 -0500

    use the background image

--- src/HelloWorld.c
+++ src/HelloWorld.c
@@ -3,6 +3,8 @@
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 static GFont s_time_font;
+static BitmapLayer  *s_background_layer;
+static GBitmap *s_background_bitmap;
 
 static void update_time() {
     // Get a tm structure
@@ -24,8 +26,14 @@ static void update_time() {
 }
 
 static void main_window_load(Window *window) {
+    // Ceate GBitmap, then set to created BitmapLayer
+    s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BACKGROUND);
+    s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
+    bitmap_layer_set_bitmap(s_background_layer,s_background_bitmap);
+    layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
+
     // Create time TextLayer
-    s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
+    s_time_layer = text_layer_create(GRect(2, 55, 144, 50));
     text_layer_set_background_color(s_time_layer, GColorClear);
     text_layer_set_text_color(s_time_layer, GColorBlack);
 
@@ -42,6 +50,8 @@ static void main_window_load(Window *window) {
 static void main_window_unload(Window *window) {
     text_layer_destroy(s_time_layer);
     fonts_unload_custom_font(s_time_font);
+    gbitmap_destroy(s_background_bitmap);
+    bitmap_layer_destroy(s_background_layer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
